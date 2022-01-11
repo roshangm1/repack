e89706fddb7e476d2eb4945c60ededde22812680
Roshan Gautam <roosan.gm@gmail.com>
2022-01-11 13:54:29 +0100

    fix: try tree shaking with lodash-es

--- packages/TesterApp/src/App.js
+++ packages/TesterApp/src/App.js
@@ -6,6 +6,10 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+import isArray from 'lodash-es/isArray'; // 1348545
+// import {isArray} from 'lodash-es'; // 1444136
+// if lodash-es is not used then 1348412
+
 import { Colors, Header } from 'react-native/Libraries/NewAppScreen';
 import DeveloperActivitySvg from './undraw_Developer_activity_re_39tg.svg';
 
@@ -19,6 +23,8 @@ const App = () => {
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+  const isThisArray = isArray([1, 2, 3]);
+  console.log('a', isThisArray);
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
